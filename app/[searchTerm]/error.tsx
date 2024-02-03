903216adcb5f639b1465e5fc5a30cfa83cdcaa29
Mohamad-jaber <mohad.jaber2001@gmail.com>
2024-02-03 23:07:16 +0200

    Change background

--- app/[searchTerm]/error.tsx
+++ app/[searchTerm]/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
     }, [error]);
 
     return (
-        <main className="bg-slate-200 mx-auto max-w-lg py-1 px-4 min-h-screen">
+        <main className="text-white mx-auto max-w-lg py-1 px-4 min-h-screen">
             <h2 className="my-4 text-2xl font-bold">Something went wrong!</h2>
             <button className="mb-4 p-4 bg-red-500 text-white rounded-xl"
                 onClick={
--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -33,7 +33,7 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
 
     const content = (
         <Suspense fallback={<h2>Loading...</h2>}>
-            <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+            <main className="text-white mx-auto max-w-lg py-1 min-h-screen">
                 {results
                     ? Object.values(results).map(result => {
                         return <Item key={result.pageid} result={result} />
